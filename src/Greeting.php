bda0f1b4138e465d9fae81d4605b8349ef4b6972
abhishek bhadoriya <abhishek.bhadoriya@samarth.ac.in>
2024-04-09 22:26:13 +0530

    Demo Package

---
+++ src/Greeting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace mypackage;
+
+class Greeting
+{
+    public static function greet()
+    {
+        return "Hello My Name Is Abhishek Bhadoriya This Is My First Package!";
+    }
+
+}
